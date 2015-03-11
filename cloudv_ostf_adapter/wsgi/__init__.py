610861eea5d67a80966dc56edceac5d864096f73
Svetlana Shturm <sshturm@mirantis.com>
2015-03-11 11:04:00 +0000

    Init REST API
    This patch adds REST API for next operations:
     - getting of list plugins (GET /v1/plugins),
     - getting of suites from plugin (GET /v1/plugin/<name>/suites)
     - getting of tests from suites
       (GET /v1/plugin/<name>/suites/<suite>/tests).
    
    Change-Id: I8a5c7a14a791d62fc856526dbd5585430ec2d555

---
+++ cloudv_ostf_adapter/wsgi/__init__.py
@@ -0,0 +1,70 @@
+#    Copyright 2015 Mirantis, Inc
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from flask.ext import restful
+from flask.ext.restful import abort
+from oslo_config import cfg
+
+from cloudv_ostf_adapter import validation_plugin
+
+
+CONF = cfg.CONF
+
+
+class BaseTests(restful.Resource):
+    def __init__(self, *args, **kwargs):
+        super(BaseTests, self).__init__(*args, **kwargs)
+        self.plugins = {}
+        for plugin in validation_plugin.VALIDATION_PLUGINS:
+            _plugin = plugin(load_tests=True)
+            self.plugins[_plugin.name] = _plugin
+
+    def _get_plugin(self, **kwargs):
+        plugin = kwargs.pop('plugin', None)
+        if plugin is None or plugin not in self.plugins:
+            abort(404,
+                  message='Unsupported plugin %s.' % plugin)
+        return self.plugins[plugin]
+
+    def _get_suite(self, plugin, suite=None):
+        if suite not in plugin.suites:
+            abort(404,
+                  message='Unknown suite %s.' % suite)
+        return suite
+
+
+class Plugins(BaseTests):
+
+    def get(self, **kwargs):
+        return {'plugins': self.plugins.keys()}
+
+
+class Plugin(BaseTests):
+
+    def get(self, **kwargs):
+        plugin = self._get_plugin(**kwargs)
+        return {'plugin': plugin.name,
+                'suites': plugin.suites}
+
+
+class Suites(BaseTests):
+
+    def get(self, **kwargs):
+        plugin = self._get_plugin(**kwargs)
+        _suite = kwargs.pop('suite', None)
+        suite = self._get_suite(plugin, suite=_suite)
+        tests = plugin.get_tests_by_suite(suite)
+        return {'plugin': plugin.name,
+                'suite': suite,
+                'tests': tests}
